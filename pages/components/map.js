901518f54cf006015b380da37acac0b456ead152
Mustapha Ghannouchi <mustapha.ghannouchi@esprit.tn>
2021-11-07 11:26:54 +0100

    changes map to Map in map.js

--- pages/components/map.js
+++ pages/components/map.js
@@ -6,24 +6,24 @@ import { useEffect } from "react";
 mapboxgl.accessToken =
   "pk.eyJ1IjoibXVzdGFwaGFnaGFubm91Y2hpIiwiYSI6ImNrdm1kbHZ1ZjllamMzMHM3NzM3czMweGQifQ.FL52UMDqPsTARFYEk4SA9w";
 
-const map = (props) => {
+const Map = (props) => {
   
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
+    const Map1 = new mapboxgl.Map({
+      container: "Map1",
       style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
       center: [9.537499, 33.886917],
       zoom: 5.2,
     });
 
     if(props.pickupCoordinates){
-      addToMap(map,props.pickupCoordinates)
+      addToMap(Map1,props.pickupCoordinates)
     }
     if(props.dropoffCoordinates){
-      addToMap(map,props.dropoffCoordinates)
+      addToMap(Map1,props.dropoffCoordinates)
     }
     if(props.pickupCoordinates && props.dropoffCoordinates){
-      map.fitBounds([
+      Map1.fitBounds([
         props.dropoffCoordinates,
         props.pickupCoordinates
       ], {
@@ -32,18 +32,18 @@ const map = (props) => {
     }
   },[props.pickupCoordinates , props.dropoffCoordinates]);
 
-  const addToMap = (map,coordinates) => {
+  const addToMap = (Map1,coordinates) => {
     const marker1 = new mapboxgl.Marker()
       .setLngLat(coordinates)
-      .addTo(map);
+      .addTo(Map1);
   };
 
-  return <Wrapper id="map">
+  return <Wrapper id="Map1">
           
   </Wrapper>;
 };
 
-export default map;
+export default Map;
 
 const Wrapper = tw.div`
 flex-1 h-1/2
